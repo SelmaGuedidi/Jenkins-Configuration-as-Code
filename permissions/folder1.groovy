7ff96751bd478f9db7128acc9f23c5e1e0b77b08
SelmaGuedidi <gueselma7@gmail.com>
2024-07-31 16:55:27 +0100

    Change folder permissions files

--- permissions/folder1.groovy
+++ permissions/folder1.groovy
@@ -1,21 +1,32 @@
-folder('folder1') {
-    properties {
-        authorization {
-            // Admin has full permissions
-            permission('hudson.model.Item.Read', 'admin')
-            permission('com.cloudbees.plugins.credentials.CredentialsProvider.Update', 'admin')
-            permission('com.cloudbees.plugins.credentials.CredentialsProvider.Create', 'admin')
-            permission('com.cloudbees.plugins.credentials.CredentialsProvider.Delete', 'admin')
-            permission('hudson.model.Item.Build', 'admin')
-            permission('hudson.model.Item.Cancel', 'admin')
-           
+import com.michelin.ci.jenkins.plugins.authmatrix.*
 
-            // User1 has read and build permissions
-            permission('hudson.model.Item.Read', 'user1')
-            permission('hudson.model.Item.Build', 'user1')
-
-            // User2 has read permission only
-            permission('hudson.model.Item.Read', 'user2')
+def setPermissions(folderName, permissionsMap) {
+    def folder = Jenkins.instance.getItemByFullName(folderName)
+    if (folder) {
+        def authorizationMatrix = folder.getProperties().find { it instanceof AuthorizationMatrixProperty }
+        if (!authorizationMatrix) {
+            authorizationMatrix = new AuthorizationMatrixProperty()
+            folder.addProperty(authorizationMatrix)
         }
+        
+        permissionsMap.each { permission, users ->
+            users.each { user ->
+                authorizationMatrix.add(permission, user)
+            }
+        }
+        folder.save()
+    } else {
+        println "Folder ${folderName} not found!"
     }
 }
+
+// Define permissions for folder1
+def permissionsFolder1 = [
+    'hudson.model.Item.READ': ['user1', 'user2'],
+    'hudson.model.Item.BUILD': ['user1'],
+    'hudson.model.Item.CONFIGURE': ['admin'],
+    'hudson.model.Item.DELETE': ['admin'],
+    'hudson.model.Item.CREATE': ['admin']
+]
+
+setPermissions('folder1', permissionsFolder1)
