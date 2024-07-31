f3332380e71ec7647797f2b385a7487d7a39de47
SelmaGuedidi <gueselma7@gmail.com>
2024-07-31 15:39:23 +0100

    Change folder permissions files

--- permissions/folder1.groovy
+++ permissions/folder1.groovy
@@ -1,16 +1,23 @@
-def folder1 = 'folder1'
+import jenkins.model.Jenkins
+import com.cloudbees.hudson.plugins.folder.properties.AuthorizationMatrixProperty
+import hudson.security.Permission
 
-authorization {
+def folder = Jenkins.instance.getItem('folder1')
+
+def property = new AuthorizationMatrixProperty([
     // Admin has full permissions
-    permission('hudson.model.Item.Read', 'admin', folder1)
-    permission('hudson.model.Item.Configure', 'admin', folder1)
-    permission('hudson.model.Item.Create', 'admin', folder1)
-    permission('hudson.model.Item.Delete', 'admin', folder1)
+    Permission.fromId('hudson.model.Item.Read'): ['admin'],
+    Permission.fromId('hudson.model.Item.Configure'): ['admin'],
+    Permission.fromId('hudson.model.Item.Create'): ['admin'],
+    Permission.fromId('hudson.model.Item.Delete'): ['admin'],
 
     // User1 has read and build permissions
-    permission('hudson.model.Item.Read', 'user1', folder1)
-    permission('hudson.model.Item.Build', 'user1', folder1)
+    Permission.fromId('hudson.model.Item.Read'): ['user1'],
+    Permission.fromId('hudson.model.Item.Build'): ['user1'],
 
     // User2 has read permission only
-    permission('hudson.model.Item.Read', 'user2', folder1)
-}
+    Permission.fromId('hudson.model.Item.Read'): ['user2']
+])
+
+folder.addProperty(property)
+folder.save()
