e570dffb8563353327626c6a69b555e95c4bef28
SelmaGuedidi <gueselma7@gmail.com>
2024-07-31 16:16:40 +0100

    Change folder permissions files

--- folders.groovy
+++ folders.groovy
@@ -1,9 +1,17 @@
 folder('folder1') {
     displayName('Folder 1')
     description('This is folder 1')
+    properties {
+        authorizationMatrix {
+        }
+    }
 }
 
 folder('folder2') {
     displayName('Folder 2')
     description('This is folder 2')
+    properties {
+        authorizationMatrix {
+        }
+    }
 }
