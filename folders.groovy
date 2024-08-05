e3c06a3947288bb5aed50fb5bc2becfbad72d583
SelmaGuedidi <gueselma7@gmail.com>
2024-08-05 14:00:13 +0100

    Fix ambiguous permissions

--- folders.groovy
+++ folders.groovy
@@ -1,23 +1,23 @@
 folder('folder1') {
     description('Folder containing the front project')
     authorization {
-         permission('USER:hudson.model.Item.Discover', 'admin')
-                 permission('USER:hudson.model.Item.Discover', 'user1')
+         permission('hudson.model.Item.Discover', 'USER:admin')
+                 permission('USER:hudson.model.Item.Discover', 'USER:user1')
             }
 }
 folder('folder2') {
     description('Folder containing the front project')
     authorization {
-                permission('USER:hudson.model.Item.Discover', 'user1')
+                permission('USER:hudson.model.Item.Discover', 'USER:user1')
             }
 }
 folder('user3-folder') {
             description('Folder for user1')
             displayName('User1 Folder')
             authorization {
-                permissions('user1', [
-            'USER:hudson.model.Item.Create',
-            'USER:hudson.model.Item.Discover'
+                permissions('USER:user1', [
+            'hudson.model.Item.Create',
+            'hudson.model.Item.Discover'
         ])
             }
         }
