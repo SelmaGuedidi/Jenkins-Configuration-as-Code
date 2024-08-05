e4377bd3fd2485d268b4d5180919f55f49c4ebb5
SelmaGuedidi <gueselma7@gmail.com>
2024-08-05 13:16:34 +0100

    Fix authorization permissions

--- folders.groovy
+++ folders.groovy
@@ -1,19 +1,14 @@
 folder('folder1') {
     description('Folder containing the front project')
     authorization {
-                permission('hudson.model.Item.Read:user1')
-                permission('hudson.model.Item.Build:user1')
-                permission('hudson.model.Item.Create:user1')
-                permission('hudson.model.Item.Delete:admin')
+         permission('hudson.model.Item.Discover', 'admin')
+                 permission('hudson.model.Item.Discover', 'user1')
             }
 }
 folder('folder2') {
     description('Folder containing the front project')
     authorization {
-                permission('hudson.model.Item.Read:user1')
-                permission('hudson.model.Item.Build:user1')
-                permission('hudson.model.Item.Create:user1')
-                permission('hudson.model.Item.Delete:admin')
+                permission('hudson.model.Item.Discover', 'user1')
             }
 }
 folder('user3-folder') {
@@ -26,3 +21,9 @@ folder('user3-folder') {
         ])
             }
         }
+
+folder('example-3') {
+    authorization {
+        permissionAll('user1')
+    }
+}
