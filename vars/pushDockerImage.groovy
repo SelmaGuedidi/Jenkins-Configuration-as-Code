bc0914a3ae7bb5017083985f8f52b748f88b61a3
SelmaGuedidi <gueselma7@gmail.com>
2024-08-26 23:08:00 +0100

    Update pushDockerImage.groovy

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -3,7 +3,7 @@ def call(String imageName, String dockerHubRepo) {
         sh '''
         echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
          docker tag ${DOCKER_IMAGE} ${DOCKERHUB_REPO}:latest'
-        docker push ${DOCKERHUB_REPO}:latest'
+        docker push ${DOCKERHUB_REPO}'
         '''
     }
 }
