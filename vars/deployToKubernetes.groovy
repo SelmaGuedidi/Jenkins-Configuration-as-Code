f1b3203afbc42ac34a1ea556c1356e858223ec4d
SelmaGuedidi <gueselma7@gmail.com>
2024-08-29 14:38:09 +0100

    Add serviceaccount + kubectl port-forward

--- vars/deployToKubernetes.groovy
+++ vars/deployToKubernetes.groovy
@@ -1,6 +1,7 @@
-def call(String deploymentYaml, String serviceYaml) {
+def call(String deploymentYaml, String serviceYaml,  String serviceName, String port , String portForward) {
     script {
         sh "kubectl apply -f ${deploymentYaml}"
         sh "kubectl apply -f ${serviceYaml}"
+        sh "kubectl port-forward service/${serviceName} ${portForward}:${port}"
     }
 }
